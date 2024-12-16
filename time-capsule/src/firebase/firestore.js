bf469b643b9e5537b793bdcbc022c4a2f6fdbf8d
JayD2003 <jayasaidodla@gmail.com>
2024-12-16 18:59:33 +0530

    removeed some eempty  files

--- time-capsule/src/firebase/firestore.js
+++
@@ -1,8 +0,0 @@
-import { getFirestore, setDoc, doc } from 'firebase/firestore';
-import app from './firebase-config';
-
-const db = getFirestore(app);
-
-export const createUserProfile = async (userId, data) => {
-  await setDoc(doc(db, 'users', userId), data);
-};
